df473d0fa2b1cf5074db4d46d220f0a5a03c2724
Benjamín Torres <benjamin.torres04@alumnos.ucn.cl>
2024-12-07 14:55:37 -0300

    IA y poda cocinadas(emoji grito y fuegos)

--- cpu.cpp
+++ cpu.cpp
@@ -1,6 +1,6 @@
 #include "cpu.h"
 #include <limits.h>
-
+#include <iostream>
 using namespace std;
 
 int Cpu::evaluar(Tablero tablero) {
@@ -13,7 +13,7 @@ int Cpu::evaluar(Tablero tablero) {
     return 0;
 }
 
-int Cpu::minimax(Tablero tablero, int profundidad, bool esMax){
+int Cpu::minimax(Tablero tablero, int profundidad, bool esMax, int alfa, int beta){
     int puntaje = evaluar(tablero);
 
     if(puntaje == 10 || puntaje == -10){
@@ -24,7 +24,7 @@ int Cpu::minimax(Tablero tablero, int profundidad, bool esMax){
         return 0;
     }
 
-    if(esMax){
+    if(esMax){ // alfa va a ser del max (de la IA)
         int mejor = INT_MIN;
 
         for(int fila = 0; fila < 3; ++fila){
@@ -32,9 +32,18 @@ int Cpu::minimax(Tablero tablero, int profundidad, bool esMax){
                 if(tablero.valido(fila,col)){
                     tablero.hacerJugada('O',fila,col);
 
-                    mejor = max(mejor,minimax(tablero, profundidad +1, false));
+                    mejor = max(mejor,minimax(tablero, profundidad +1, false, alfa, beta));
+                    alfa = max(alfa,mejor);
 
+                    
+                    
+                    
                     tablero.hacerJugada(' ',fila,col);
+
+                    if (beta <= alfa) { //Poda
+                    
+                    break;
+                    }
                 }
             }
         }
@@ -48,30 +57,42 @@ int Cpu::minimax(Tablero tablero, int profundidad, bool esMax){
                 if(tablero.valido(fila,col)){
                     tablero.hacerJugada('X',fila,col);
 
-                    peor = min(peor,minimax(tablero, profundidad +1, true));
-
+                    peor = min(peor,minimax(tablero, profundidad +1, true, alfa, beta));
+                    beta = min(beta, peor);
+                    
+                    
+                    
                     tablero.hacerJugada(' ',fila,col);
+
+                    if (beta <= alfa) { //Poda
+                    
+                    break;
+                    }
                 }
             }
         }
+       
         return peor;
     }
 }
 pair<int,int> Cpu::mejorJugada(Tablero tablero){
     int mejorValor = INT_MIN;
     pair<int,int> mejorMov = {-1,-1};
-
+    int cont = 0;
     for(int fila = 0; fila < 3; ++fila){
         for(int col = 0; col < 3; ++col){
             if(tablero.valido(fila,col)){
                 //Se simula la jugada
                 tablero.hacerJugada('O',fila,col);
-
-                int valor = minimax(tablero, 0, false);
-
+                
+                int valor = minimax(tablero, 0, false, INT_MIN, INT_MAX);
+                
                 tablero.hacerJugada(' ', fila,col);
-
-                if(valor > mejorValor) {
+                    cout << valor <<endl;
+                    cout << fila << ", " << col << endl;
+                    cout << "" << endl;
+                if(valor >= mejorValor) {
+                    
                     mejorValor = valor;
                     mejorMov = {fila,col};
                 }
--- cpu.h
+++ cpu.h
@@ -8,7 +8,7 @@ class Cpu{
     
     char tablero[3][3];
     int evaluar(Tablero tablero);
-    int minimax(Tablero tablero, int profundidad, bool esMax);
+    int minimax(Tablero tablero, int profundidad, bool esMax, int alfa, int beta);
 
     public:
     std::pair<int,int> mejorJugada(Tablero tablero);
